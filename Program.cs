01bfcd11ff564bb5cb57bb31d7de0928839e23ff
GabrielAlves5 <ecgabriel.alves@gmail.com>
2025-03-17 10:33:30 -0300

    Agora eu sei meu nome

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿string nome, sobrenome;
+Console.Write("Insira seu nome: ");
+nome = Console.ReadLine()!;
+Console.WriteLine("Insira seu sobrenome: ");
+sobrenome = Console.ReadLine()!;
+Console.WriteLine($"Seu nome é {nome} {sobrenome}");
+Console.WriteLine($"Seu nome de catálogo {sobrenome.ToUpper()},{nome}");
